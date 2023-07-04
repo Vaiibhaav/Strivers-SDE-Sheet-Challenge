c7fdd50fef9bb938aff2802745f7d885fb007fb3
Vaiibhaav <vaibhavsaxena533@gmail.com>
2023-07-04 21:07:24 +0530

    ME(n/2)

---
+++ majorityElement(n/2).cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int num=nums[0],count=1;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]==num)count++;
+            else{
+                count--;
+                if(count==0){
+                    num=nums[i];
+                    count=1;
+                }
+            }
+        }
+        return num;
+    }
+};
